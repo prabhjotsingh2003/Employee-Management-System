9a090d9077c6b1956fd593a7791169b6e71d34cf
prabhjotsingh2003 <prabhjot6280472@gmail.com>
2023-10-21 17:37:45 +0530

    commit done

--- js/Main.js
+++ js/Main.js
@@ -34,7 +34,6 @@ var imgUrl;
 /* end all global variable*/
 
 /* start register coding */
-
 registerForm.onsubmit = function (e) {
   e.preventDefault();
   registrationData();
@@ -42,6 +41,7 @@ registerForm.onsubmit = function (e) {
   registerForm.reset("");
   closeBtn.click();
 };
+
 if (localStorage.getItem("userData") != null) {
   userData = JSON.parse(localStorage.getItem("userData"));
 }
@@ -61,3 +61,96 @@ function registrationData() {
   localStorage.setItem("userData", userString);
   swal("Good job!", "Registration Success!", "success");
 }
+
+//start returning data on page from localstorage
+var tableData = document.querySelector("#table-data");
+const getDataFromLocal = () => {
+  tableData.innerHTML = "";
+  userData.forEach((data, index) => {
+    tableData.innerHTML += `  
+    <tr index='${index}'>
+          <td>${index + 1}</td>
+          <td><img src="${data.profilePic}" width="40"></td>
+          <td>${data.id}</td>
+          <td>${data.name}</td>
+          <td>${data.l_name}</td>
+          <td>${data.email}</td>
+          <td>${data.officeCode}</td>
+          <td>${data.jobTitle}</td>
+          <td>
+            <button class="edit-btn"><i class="fa fa-eye"></i></button>
+            <button class="del-btn" style="background-color: #EE534F;"><i class="fa fa-trash"></i></button></td>
+         </tr>   
+    `;
+  });
+
+  /* start delete coding */
+  var i;
+  var allDelBtn = document.querySelectorAll(".del-btn");
+  for (i = 0; i < allDelBtn.length; i++) {
+    allDelBtn[i].onclick = function () {
+      var tr = this.parentElement.parentElement;
+      var id = tr.getAttribute("index");
+      swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+          userData.splice(id, 1);
+          localStorage.setItem("userData", JSON.stringify(userData));
+          tr.remove();
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+    };
+  }
+  // start update coding
+  var allEdit = document.querySelectorAll(".edit-btn");
+  for (i = 0; i < allEdit.length; i++) {
+    allEdit[i].onclick = function () {
+      var tr = this.parentElement.parentElement;
+      var td = tr.getElementsByTagName("TD");
+      var index = tr.getAttribute("index");
+      var imgTag = td[1].getElementsByTagName("IMG");
+      var profilePic = imgTag[0].src;
+      var id = td[2].innerHTML;
+      var name = td[3].innerHTML;
+      var l_name = td[4].innerHTML;
+      var email = td[5].innerHTML;
+      var officeCode = td[6].innerHTML;
+      var jobTitle = td[7].innerHTML;
+      addBtn.click();
+      registerBtn.disabled = true;
+      updateBtn.disabled = false;
+      idEl.value = id;
+      nameEl.value = name;
+      l_nameEl.value = l_name;
+      emailEl.value = email;
+      officeEl.value = officeCode;
+      jobTitleEl.value = jobTitle;
+      if (imgTag) {
+        profile_pic.src = profilePic;
+      }
+      updateBtn.onclick = function (e) {
+        userData[index] = {
+          id: idEl.value,
+          name: nameEl.value,
+          l_name: l_nameEl.value,
+          email: emailEl.value,
+
+          officeCode: officeEl.value,
+          jobTitle: jobTitleEl.value,
+          profilePic: uploadPic.value == "" ? profile_pic.src : imgUrl,
+        };
+        localStorage.setItem("userData", JSON.stringify(userData));
+      };
+    };
+  }
+};
