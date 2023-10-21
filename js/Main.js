c6ecf1bb3076c1380df914ebdbdbf46dd4e7656d
prabhjotsingh2003 <prabhjot6280472@gmail.com>
2023-10-21 19:59:27 +0530

    adding clearing all data

--- js/Main.js
+++ js/Main.js
@@ -111,6 +111,7 @@ const getDataFromLocal = () => {
       });
     };
   }
+
   // start update coding
   var allEdit = document.querySelectorAll(".edit-btn");
   for (i = 0; i < allEdit.length; i++) {
@@ -154,3 +155,87 @@ const getDataFromLocal = () => {
     };
   }
 };
+getDataFromLocal();
+
+// image processing
+uploadPic.onchange = function () {
+  if (uploadPic.files[0].size < 1000000) {
+    var fReader = new FileReader();
+    fReader.onload = function (e) {
+      imgUrl = e.target.result;
+      profile_pic.src = imgUrl;
+      console.log(imgUrl);
+    };
+    fReader.readAsDataURL(uploadPic.files[0]);
+  } else {
+    alert("File Size Is Too Large");
+  }
+}
+
+//start search coding
+
+var searchEl = document.querySelector("#empId");
+searchEl.oninput = function () {
+  searchFun();
+}
+function searchFuc() {
+  var tr = tableData.querySelectorAll("TR");
+  var id = searchEl.value.toLowerCase();
+  var i;
+  for (i = 0; i < tr.length; i++){
+    var id = tr[i].getElementsByTagName("TD")[2].innerHTML;
+    var name = tr[i].getElementsByTagName("TD")[3].innerHTML;
+    var l_name = tr[i].getElementsByTagName("TD")[4].innerHTML;
+    var email = tr[i].getElementsByTagName("TD")[5].innerHTML;
+    var officode = tr[i].getElementsByTagName("TD")[6].innerHTML;
+    var jobtitle = tr[i].getElementsByTagName("TD")[7].innerHTML;
+    if (id.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else if (name.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else if (l_name.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else if (email.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else if (officecode.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else if (jobtitle.toLowerCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    }
+    else {
+      tr[i].style.display = "none";
+    }
+    
+  }
+}
+// start clear all data
+var dellAllBtn = document.querySelector("#del-all-btn");
+var allDelBox = document.querySelector("#del-all-btn");
+dellAllBtn.addEventListene('click', () => {
+  if (allDelBox.checked == true) {
+     swal({
+       title: "Are you sure?",
+       text: "Once deleted, you will not be able to recover this imaginary file!",
+       icon: "warning",
+       buttons: true,
+       dangerMode: true,
+     }).then((willDelete) => {
+       if (willDelete) { 
+         swal("Poof! Your imaginary file has been deleted!", {
+           icon: "success",
+         });
+       } else {
+         swal("Your imaginary file is safe!");
+       }
+     });
+  }
+  else {
+    swal("Check The Box", "Please check the box to delete data", "warning");
+  }
+}
+)
